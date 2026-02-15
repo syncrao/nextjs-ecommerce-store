da49489759937740594cfeedd4e40d5ab4e468ef
SyncRao <shahrukhrao1@gmail.com>
2026-02-15 23:38:59 +0530

    added mongoDB

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,5 +1,5 @@
 async function getUsers() {
-  const res = await fetch("http://localhost:3000/api/user", {
+  const res = await fetch("https://bandhej.vercel.app/api/user", {
     cache: "no-store",
   });
 
